f89822031397c1d1b3e8427a3d38b8e72b06b941
Ai01 <2911403289@qq.com>
2016-07-18 15:44:24 +0800

    true thum

--- views/photo.js
+++ views/photo.js
@@ -34,6 +34,8 @@ window.onload = function() {
         //console.log(fileEnter.files[0].size);
         //文件上传实现
         var files = fileEnter.files;
+        console.log(files[0]);
+        console.log(files[0].webkitRelativePath)
         var formData = new FormData();
         for (var i = 0; i < files.length; i++) {
             formData.append(files[i].name, files[i]);
@@ -53,7 +55,8 @@ window.onload = function() {
             if (event.lengthComputable) {
                 var completedPercent = event.loaded / event.total;
                 //console.log(completedPercent);
-                container.innerHTML="<div style='margin:10px;'><span>"+files[0].name+"</span><br><progress value='"+completedPercent+"' max='1'></progress></div>";
+                container.innerHTML="<div style='margin:10px;'>"+"<img style='height:50px;width:50px;' src="+window.URL.createObjectURL(files[0])+"><br>"+
+                "<span>"+files[0].name+"</span><br><progress value='"+completedPercent+"' max='1'></progress></div>";
             }
         };
         xhr.timeout = 3000;
